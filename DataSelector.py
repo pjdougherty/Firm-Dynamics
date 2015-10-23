fed6111f75ad9c00cb0b564d9c0c39bc62c85fca
pjdougherty <pjdougherty@outlook.com>
2015-10-23 16:34:28 -0400

    Streamlined selection and summation
    Having forgotten that it existed, I rewrote this script, but did it
    better. It also now includes a col_names() function that returns NETS
    column names so that the same chunk of code doesn't need to be
    copy/pasted into every new script.

--- DataSelector.py
+++ DataSelector.py
@@ -1,84 +1,100 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep 02 16:58:34 2015
+Created on Thu Oct 22 11:43:36 2015
 
 @author: pdougherty
 
-This script selects employment and sales data from a read-in NETS data or other dataframe with this structure:
+This script is designed to select sales and employment figures from a dataframe of NETS data. It includes relevant header names in col_names.
 
- Emp01 | Emp02 | Emp03 | ... | Sales01 | Sales02 | Sales03 | ...
--------+-------+-------+ ... +---------+---------+---------+ ...
- 100   | 150   | 155   | ... | 10000   | 15000   | 15000   | ...
- 
-Company information is left out, and can be added to data = {} dictionary constructors if needed.
-
-May be useful to combine this with a latitude and longitude selector so that coordinated only for companies in Maryland are selected. At the same time, it's possible that companies without OriginState or DestState records of "MD" but exist in Maryland would be omitted.
-
-Useful for plotting sales and employment over time in a given industry.
+getMDData() is designed to select sales and employment figures for firms during the time that they are located in the State of Maryland.
+sumMDData() generates two data frames, one for employment and one for sales, with aggregated Maryland data, organized by year. The column containing year values is in datetime format.
 """
-
 import pandas as pd
 import numpy as np
 
-# Get only companies in Maryland at the time
-def selectData(df, year):
+def col_names():
     '''
-    parameters:
-        - df: dataframe to have information pulled from. Typically an dataframe with information from one industry.
-        - year: the year for the coordinates to represent. Point representation changes with relocations, openings, and closings.
+    Returns a list of column names for the standard NETS database and for the NETS database joined with the NETS relocation database.
     '''
-    
-    data_dict = {}
-
-    for i, estab in df.iterrows():
-        if df['FirstYear'][i] <= year and df["LastYear"][i] >= year:
-            # If company started in MD and never moved
-            if df['OriginState'][i] == 'MD' and np.isnan(df['LastMove'][i]):
-                data = {'employment':df['{}'.format('Emp'+str(year)[2:4])][i],
-                             'sales':df['{}'.format('Sales'+str(year)[2:4])][i],
-                             'year':year}
-                data_dict['co'+str(i)] = data
-            # If company started in MD and moved after the passed year
-            elif df['OriginState'][i] == 'MD' and df['LastMove'][i] >= year:
-                data = {'employment':df['{}'.format('Emp'+str(year)[2:4])][i],
-                             'sales':df['{}'.format('Sales'+str(year)[2:4])][i],
-                             'year':year}
-                data_dict['co'+str(i)] = data
-            # If the company moved after the passed year and ended up in Maryland
-            elif df['LastMove'][i] < year and df['DestState'][i] == 'MD':
-                data = {'employment':df['{}'.format('Emp'+str(year)[2:4])][i],
-                             'sales':df['{}'.format('Sales'+str(year)[2:4])][i],
-                             'year':year}
-                data_dict['co'+str(i)] = data
-            else:
-                pass
-                
-    df = pd.DataFrame(data_dict).T.reset_index(drop=True)
-    
-    return df
-
-# Get employment and sales for all years
-def allData(df, first_year, last_year):
-    '''
-    parameters:
-        - df:dataframe to have information pulled from. Typically an dataframe with information from one industry.
-        - first_year: the first year of data to be represented. Must be 1990 or later.
-        - last_year: the most recent year of data to be represented. Must be 2012 or earlier.
-    '''
-    if first_year >= 1990 and last_year <= 2013:
-        pass
-    elif first_year < 1990:
-        print 'First year must be 1990 or later.'
-    elif last_year > 2013:
-        print 'Last year must be 2013 or earlier. Data is available through 2012.'
-    else:
-        pass
-    
-    dfs = []
-    for y in range(first_year, last_year):
-        d = selectData(df, y)
-        dfs.append(d)
+    col_names = pd.read_csv(r'G:\Publications\2015\Regional Clustering\NETS2012_MD_EAGB.txt', nrows=1, delimiter='\t')
+    col_names = list(col_names.columns)
+    # Turn this on ONLY when an EAGB industry has been assigned to each company
+    col_names.append('EAGBIndustry')
+    move_col_names = 'DunsNumber	MoveYear	Company	TradeName	MoveSIC	OriginCity	OriginState	OriginZIP	OriginFIPSCounty	OriginCounty	DestCity	DestState	DestZIP	DestFIPSCounty	DestCounty	MoveEmp	EmpC	MoveSales	MoveSalesC	MoveOften	Active	SizeCat	EstCat	Subsidiary	OriginLatitude	OriginLongitude	OriginLevelCode	DestLatitude	DestLongitude	DestLevelCode	Distance'
+    move_col_names = move_col_names.split('\t')
+    move_col_names = list(move_col_names)
+    for i in range(0, len(move_col_names)):
+        move_col_names[i] = move_col_names[i] + '_r'
         
-    data_df = pd.concat([frame for frame in dfs])
+    return col_names, move_col_names
+
+def getMDData(g):
+    '''
+    getMDData() is designed to select sales and employment figures for firms during the time that they are located in the State of Maryland.
     
-    return data_df
+    parameters:
+        - g: data frame from NETS database with column headers
+    '''
+    emp_dict = {k:[] for k in range(1990,2013)}
+    sales_dict = {k:[] for k in range(1990,2013)}
+    
+    first00 = int(sum(emp_dict[2000]))
+
+    for i, estab in g.iterrows():
+        if np.isnan(g['LastMove'][i]) and g['State_First'][i]=='MD':
+            for k in emp_dict.keys():
+                emp_dict[k].append(g['{}'.format('Emp'+str(k)[2:4])][i])
+            for k in sales_dict.keys():
+                sales_dict[k].append(g['{}'.format('Sales'+str(k)[2:4])][i])
+        else:
+            if g['State_First'][i] == 'MD' and g['DestState'][i]=='MD':
+                for k in emp_dict.keys():
+                    emp_dict[k].append(g['{}'.format('Emp'+str(k)[2:4])][i])
+                for k in sales_dict.keys():
+                    sales_dict[k].append(g['{}'.format('Sales'+str(k)[2:4])][i])
+            else:
+                year = g['LastMove'][i]
+                if g['State_First'][i] == 'MD':
+                    emp_years = range(1990,int(year+1))
+                    sales_years = range(1990,int(year+1))
+                    for k in emp_years:
+                        emp_dict[k].append(g['{}'.format('Emp'+str(k)[2:4])][i])
+                    for k in sales_years:
+                        sales_dict[k].append(g['{}'.format('Sales'+str(k)[2:4])][i])
+                elif g['DestState'][i]=='MD':
+                    emp_years = range(int(year),2013)
+                    sales_years = range(int(year),2013)
+                    for k in emp_years:
+                        emp_dict[k].append(g['{}'.format('Emp'+str(k)[2:4])][i])
+                    for k in sales_years:
+                        sales_dict[k].append(g['{}'.format('Sales'+str(k)[2:4])][i])
+                        
+    last00 = int(sum(emp_dict[2000]))
+    
+    if first00==last00:
+        print 'There seems to be an issue. No new data was added to the dataframe - unless nobody in Maryland was employed in this industry in 2000.'
+    
+    return emp_dict, sales_dict
+
+def sumMDData(emp_dict, sales_dict):
+    '''
+    sumMDData() generates two data frames, one for employment and one for sales, with aggregated Maryland data, organized by year. The column containing year values is in datetime format.
+    
+    parameters:
+        - emp_dict: dictionary of employment valeus for Maryland companies organized by year. Keys must be integers.
+        - sales_dict: dictionary of employment valeus for Maryland companies organized by year. Keys must be integers.
+    '''
+    e={}
+    s={}
+    for k in emp_dict.keys():
+        e[k] = float(sum(emp_dict[k]))
+    for k in sales_dict.keys():
+        s[k] = [n for n in sales_dict[k] if np.isnan(n)==False]
+        s[k] = float(sum(s[k]))
+        
+    e = pd.DataFrame(e, index=[0]).T.reset_index().rename(columns={'index':'year', 0:'employment'})
+    e['year'] = pd.to_datetime(e.year, format='%Y')
+    s = pd.DataFrame(s, index=[0]).T.reset_index().rename(columns={'index':'year', 0:'sales'})
+    s['year'] = pd.to_datetime(s.year, format='%Y')
+
+    return e, s
